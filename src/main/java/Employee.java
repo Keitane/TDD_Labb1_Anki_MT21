c652ffe92661d8fae73b26a3c8c25da74469aa5d
Keitane <ann.christine.phan@outlook.com>
2022-02-15 11:00:38 +0100

    Refactor

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -29,7 +29,7 @@ public class Employee {
 
     }
 
-    public void setLastName(String expected) {
-        lastName = expected;
+    public void setLastName(String newLastName) {
+        lastName = newLastName;
     }
 }
