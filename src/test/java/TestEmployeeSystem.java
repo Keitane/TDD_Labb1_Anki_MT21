9a937c2d5ba89062c84c0dd802f24ef6129f73c6
Keitane <ann.christine.phan@outlook.com>
2022-02-15 13:15:04 +0100

    Red

--- src/test/java/TestEmployeeSystem.java
+++ src/test/java/TestEmployeeSystem.java
@@ -1,31 +1,36 @@
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.*;
 
 public class TestEmployeeSystem {
     Employee testObject1;
+    EmployeeSystem testObjectSystem;
 
 
     @BeforeAll
     public static void beforeAll() {
-        System.out.println("Nu börjar testsviten för Labb 1 i TDD.");
+        System.out.println("Nu börjar testsviten för Labb 1 i TDD!");
     }
 
     @AfterAll
     public static void afterAll() {
-        System.out.println("Nu avslutas testsviten.");
+        System.out.println("Nu avslutas testsviten!");
     }
 
     @AfterEach
     public void afterEach() {
-        System.out.println("Test slut.");
+        System.out.println("Test slut!");
     }
 
     @BeforeEach
     public void beforeEach() {
-        System.out.println("Test start.");
+        System.out.println("Test start!");
 
         testObject1 = new Employee("Anki", "Phan", 24, 30500.00);
+    }
+
+    @Test
+    public void checkGetEmployeeListTest() {
+        int expected = 3;
+
+        int actual =
     }
 }
