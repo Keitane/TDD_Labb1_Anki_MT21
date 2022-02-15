fce499dbf2ae9ad1d6a5d6f11a8fdf4781b7ee0b
Keitane <ann.christine.phan@outlook.com>
2022-02-15 10:23:50 +0100

    Refactor

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -4,7 +4,8 @@ public class Employee {
     private static int counter;
 
     public Employee(String name, String lastName, int age, double salary) {
-
+    counter = counter + 1;
+    ID = counter;
     }
 
     public int getUniqueID() {
